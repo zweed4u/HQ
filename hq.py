f8371e6b569866625110f77e059f4a06a00cb571
Zachary Weeden <zdw7287@rit.edu>
2018-03-22 14:30:19 -0400

    Added sms authentication process for those without tokens

--- hq.py
+++ hq.py
@@ -6,7 +6,11 @@ import configparser
 
 
 class HQ:
-    def __init__(self, bearer_token):
+    def __init__(self, phone_number, bearer_token=None):
+        self.api_base_url = 'https://api-quiz.hype.space/'
+        if bearer_token is None:
+            authentication_response = self.authenticate(phone_number)
+            bearer_token = authentication_response['auth']['authToken']
         self.config_headers = {
             'Host':             'api-quiz.hype.space',
             'Accept':           '*/*',
@@ -18,11 +22,35 @@ class HQ:
             'Authorization':    f'Bearer {bearer_token}',
             'x-hq-client':      'iOS/1.2.19 b79'
         }
-        self.api_base_url = 'https://api-quiz.hype.space/'
 
     def make_request(self, method, endpoint, params=None, headers=None, payload=None, json=None):
         response = requests.request(method, f'{self.api_base_url}{endpoint}', params=params, headers=headers, data=payload, json=json)
         return response.json()
+
+    def authenticate(self, number):
+        headers = {
+            'Host':             'api-quiz.hype.space',
+            'Content-Type':     'application/json',
+            'Accept':           '*/*',
+            'Connection':       'keep-alive',
+            'User-Agent':       'HQ/1.2.19 (co.intermedialabs.hq; build:79; iOS 10.2.0) Alamofire/4.6.0',
+            'Accept-Language':  'en-US;q=1.0',
+            'x-hq-client':      'iOS/1.2.19 b79',
+            'Accept-Encoding':  'gzip;q=1.0, compress;q=0.5'
+        }
+        json_data = {
+            'method': 'sms',
+            'phone':  f'+1{number}'
+        }
+        verification_response = self.make_request('POST', 'verifications', headers=headers, json=json_data)
+        verification_id = verification_response['verificationId']
+
+        sms_code = input('Enter SMS code sent: ')
+        json_data = {
+            'code':  sms_code
+        }
+        verification_sms_response = self.make_request('POST', f'verifications/{verification_id}', headers=headers, json=json_data)
+        return verification_sms_response
 
     def get_config(self):
         config_response = self.make_request('GET', 'config', headers=self.config_headers)
@@ -43,4 +71,9 @@ c = configparser.ConfigParser()
 configFilePath = os.path.join(root_directory, 'config.cfg')
 c.read(configFilePath)
 
-print(json.dumps(HQ(c.get('authorization', 'token')).get_me(), indent=4))
+try:
+    bearer_token = c.get('authorization', 'token')
+except:
+    bearer_token = None
+phone_number = str(c.get('verification', 'phone'))
+print(json.dumps(HQ(phone_number, bearer_token).get_me(), indent=4))
